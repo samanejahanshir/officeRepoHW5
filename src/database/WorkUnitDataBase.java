ad16a209721e5fe374f3194668fcb5488430c72a
Mahdi-Jhr <jahanshir.sm92@gmail.com>
2021-09-26 16:26:19 +0330

    modify WorkUnitDataBase class

--- src/database/WorkUnitDataBase.java
+++ src/database/WorkUnitDataBase.java
@@ -1,6 +1,5 @@
 package database;
 
-import office.Employee;
 import office.WorkUnit;
 
 import java.sql.*;
@@ -17,8 +16,8 @@ public class WorkUnitDataBase {
     public int save(WorkUnit workUnit) throws SQLException {
         if (connection != null) {
             Statement statement = connection.createStatement();
-            String sqlQuery = String.format("INSERT INTO  work_unit ( id_work_unit , name , phone_number , id_employees ) VALUES (%d,%s,%s,%s)",
-                    workUnit.getId(), workUnit.getName(), workUnit.getPhoneNumber(), workUnit.getEmployees());
+            String sqlQuery = String.format("INSERT INTO  work_unit (name,phone_number) VALUES ('%s','%s')",
+                    workUnit.getName(), workUnit.getPhoneNumber());
             int i = statement.executeUpdate(sqlQuery);
             return i;
 
@@ -30,7 +29,7 @@ public class WorkUnitDataBase {
     public int updateWorkUnitInformation(WorkUnit workUnit) throws SQLException {
         if (connection != null) {
             Statement statement = connection.createStatement();
-            String sqlQuery = String.format("UPDATE work_unit SET name=%s WHERE id_work_unit=%d", workUnit.getName(), workUnit.getId());
+            String sqlQuery = String.format("UPDATE work_unit SET name='%s' WHERE id_work_unit=%d", workUnit.getName(), workUnit.getId());
             int i = statement.executeUpdate(sqlQuery);
             return i;
 
@@ -39,15 +38,22 @@ public class WorkUnitDataBase {
         }
     }
 
-    public void showWorkUnit() throws SQLException {
+    public WorkUnit[] returnWorkUnit() throws SQLException {
+        WorkUnit[] workUnits = new WorkUnit[50];
         if (connection != null) {
             Statement statement = connection.createStatement();
             String sqlQuery = String.format("SELECT * FROM work_unit");
             ResultSet resultSet = statement.executeQuery(sqlQuery);
             if (resultSet.next()) {
-                while (resultSet.next()) {
-                    System.out.println(resultSet.getString("name"));
-                }
+                int index = 0;
+                do {
+                    workUnits[index] = new WorkUnit(resultSet.getString(2), resultSet.getString(3));
+                    workUnits[index].setId(Integer.parseInt(resultSet.getString(1)));
+                    workUnits[index].setIdEmployees(resultSet.getString(4)+"");
+                    index++;
+
+                }while (resultSet.next());
+                return workUnits;
             } else {
                 System.out.println("there aren't any thing to show ! ");
 
@@ -56,10 +62,10 @@ public class WorkUnitDataBase {
             System.out.println("connection is null !! ");
 
         }
-
+        return null;
     }
 
-    public  boolean searchWorkUnitId(int workUnitId) throws SQLException {
+    public boolean searchWorkUnitId(int workUnitId) throws SQLException {
         if (connection != null) {
             Statement statement = connection.createStatement();
             String sqlQuery = String.format("SELECT id_work_unit FROM work_unit WHERE id_work_unit=%d", workUnitId);
@@ -72,6 +78,41 @@ public class WorkUnitDataBase {
             }
         } else {
             return false;
+        }
+    }
+
+    public int updateListEmployee(WorkUnit workUnit) throws SQLException {
+        if (connection != null) {
+            Statement statement = connection.createStatement();
+            String sqlQuery = String.format("UPDATE work_unit SET id_employees='%s' WHERE id_work_unit=%d", workUnit.getIdEmployees(), workUnit.getId());
+            int i = statement.executeUpdate(sqlQuery);
+            return i;
+
+        } else {
+            return 0;
+        }
+
+
+    }
+
+    public void showWorkUnit() throws SQLException {
+        if (connection != null) {
+            Statement statement = connection.createStatement();
+            String sqlQuery = String.format("SELECT * FROM work_unit");
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+            if (resultSet.next()) {
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("name"));
+
+                }
+
+            } else {
+                System.out.println("there aren't any thing to show ! ");
+
+            }
+        } else {
+            System.out.println("connection is null !! ");
+
         }
     }
 }
