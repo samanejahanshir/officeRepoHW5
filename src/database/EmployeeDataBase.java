78b21047466cf894228a77c1f03b5fa7b4ec9c26
Mahdi-Jhr <jahanshir.sm92@gmail.com>
2021-09-28 21:31:55 +0330

    modify EmployeeDataBase  and delete constractor and extend from DataBase

--- src/database/EmployeeDataBase.java
+++ src/database/EmployeeDataBase.java
@@ -4,24 +4,22 @@ import office.Employee;
 
 import java.sql.*;
 
-public class EmployeeDataBase {
-    private Connection connection = null;
+public class EmployeeDataBase extends DataBase {
 
     public EmployeeDataBase() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/officedatabase", "root", "1234567890");
+        super();
     }
 
     public int save(Employee employee) throws SQLException {
-        if (connection != null) {
-            Statement statement = connection.createStatement();
+        if (getConnection() != null) {
+            Statement statement = getConnection().createStatement();
             String sqlQuery = String.format("INSERT INTO employee (first_name , last_name , personal_number , birth_date , id_work_unit) VALUES ('%s','%s','%s','%s',%d)",
                     employee.getFirstName(), employee.getLastName(), employee.getPersonalId(), employee.getBirthDate(), employee.getWorkUnit_id());
             int i = statement.executeUpdate(sqlQuery);
             if (i != 0) {
                 int id = searchEmployee(employee.getFirstName(), employee.getLastName(), employee.getPersonalId());
                 return id;
-            }else {
+            } else {
                 return 0;
             }
         } else {
@@ -30,8 +28,8 @@ public class EmployeeDataBase {
     }
 
     public int updateEmployeeInformation(String firstName, String lastName, int id) throws SQLException {
-        if (connection != null) {
-            Statement statement = connection.createStatement();
+        if (getConnection() != null) {
+            Statement statement = getConnection().createStatement();
             String sqlQuery = String.format("UPDATE employee SET first_name='%s' , last_name='%s' WHERE id_employee=%d", firstName, lastName, id);
             int i = statement.executeUpdate(sqlQuery);
             return i;
@@ -42,16 +40,16 @@ public class EmployeeDataBase {
     }
 
     public void showEmployee(int workUnitId) throws SQLException {
-        if (connection != null) {
-            Statement statement = connection.createStatement();
+        if (getConnection() != null) {
+            Statement statement = getConnection().createStatement();
             String sqlQuery = String.format("SELECT * FROM employee WHERE id_work_unit=%d", workUnitId);
             ResultSet resultSet = statement.executeQuery(sqlQuery);
             if (resultSet.next()) {
-                System.out.println("---------- Employees in work unit "+workUnitId+" ----------");
+                System.out.println("---------- Employees in work unit " + workUnitId + " ----------");
                 System.out.println("Id  F-Name  L-Name  P-Num  BirthDate  WorkUnit");
 
                 do {
-                    System.out.println(resultSet.getString(1)+"   "+resultSet.getString(2)+"   "+resultSet.getString(3)+"   "+resultSet.getString(4)+"   " +resultSet.getString(5)+"   "+resultSet.getString(6));
+                    System.out.println(resultSet.getString(1) + "   " + resultSet.getString(2) + "   " + resultSet.getString(3) + "   " + resultSet.getString(4) + "   " + resultSet.getString(5) + "   " + resultSet.getString(6));
                 } while (resultSet.next());
                 System.out.println("----------------------------------------------");
 
@@ -67,8 +65,8 @@ public class EmployeeDataBase {
     }
 
     public boolean searchEmployee(int idEmployee) throws SQLException {
-        if (connection != null) {
-            Statement statement = connection.createStatement();
+        if (getConnection() != null) {
+            Statement statement = getConnection().createStatement();
             String sqlQuery = String.format("SELECT id_employee FROM employee WHERE id_employee=%d", idEmployee);
             ResultSet resultSet = statement.executeQuery(sqlQuery);
             if (resultSet.next()) {
@@ -82,8 +80,8 @@ public class EmployeeDataBase {
     }
 
     public int searchEmployee(String firstName, String lastName, String personalId) throws SQLException {
-        if (connection != null) {
-            Statement statement = connection.createStatement();
+        if (getConnection() != null) {
+            Statement statement = getConnection().createStatement();
             String sqlQuery = String.format("SELECT id_employee FROM employee WHERE first_name='%s' && last_name='%s' && personal_number='%s' ", firstName, lastName, personalId);
             ResultSet resultSet = statement.executeQuery(sqlQuery);
             if (resultSet.next()) {
